b34e9310b783956f8fa4f3ce4fa39282d9707991
Siavash Tabrizian <30736084+siavashtab@users.noreply.github.com>
2019-10-09 10:30:45 -0500

    Update CNN.py

--- src/CNN.py
+++ src/CNN.py
@@ -39,106 +39,112 @@ from sklearn.metrics import f1_score
 
 ## Optimizer colleciton:
 optzer = []
+optzer_name = []
 #-------------------------------------------------------------------------------
 from keras.optimizers import Adam
 ##ref : https://arxiv.org/pdf/1412.6980v8.pdf
 optzer.append(Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False))
+optzer_name.append('Adam')
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 from keras.optimizers import SGD
 optzer.append(SGD(learning_rate=0.01, momentum=0.0, nesterov=False))
+optzer_name.append('SGD')
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 from keras.optimizers import RMSprop
 ## ref: http://www.cs.toronto.edu/~tijmen/csc321/slides/lecture_slides_lec6.pdf
 optzer.append(RMSprop(learning_rate=0.001, rho=0.9))
+optzer_name.append('RMSprop')
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 from keras.optimizers import Adagrad
 ## ref: http://www.jmlr.org/papers/volume12/duchi11a/duchi11a.pdf
 optzer.append(Adagrad(learning_rate=0.01))
+optzer_name.append('Adagrad')
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 from keras.optimizers import Adadelta
 ## ref: https://arxiv.org/pdf/1212.5701.pdf
 optzer.append(Adadelta(learning_rate=1.0, rho=0.95))
+optzer_name.append('Adadelta')
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 from keras.optimizers import Adamax
 ## ref: https://arxiv.org/pdf/1412.6980v8.pdf
 optzer.append(Adamax(learning_rate=0.002, beta_1=0.9, beta_2=0.999))
+optzer_name.append('Adamax')
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 from keras.optimizers import Nadam
 ## ref: http://cs229.stanford.edu/proj2015/054_report.pdf
 optzer.append(Nadam(learning_rate=0.002, beta_1=0.9, beta_2=0.999))
+optzer_name.append('Nadam')
 #-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-from keras.optimizers import Nadam
-## ref: http://cs229.stanford.edu/proj2015/054_report.pdf
-optzer.append(Nadam(learning_rate=0.002, beta_1=0.9, beta_2=0.999))
-#-------------------------------------------------------------------------------
+
 
 seed = 7
 np.random.seed(seed)
-for nsplt in range(9):
-    kfold = StratifiedKFold(n_splits=nsplt + 2, shuffle=True, random_state=seed)
-    cvscores = []
-    History = []
-    F1_score = []
-    for train, test in kfold.split(X, Y):
-      
-      model = Sequential()
-      model.add( Conv2D(input_shape = (32, 32,3) , filters = 32 , kernel_size=(3,3) , padding = 'same' , activation = 'relu' , kernel_initializer= 'he_uniform'))
-      model.add(BatchNormalization())
-      model.add( Conv2D(filters = 32 , kernel_size = (3,3) , padding = 'same' , activation = 'relu' , kernel_initializer = 'he_uniform'))
-      model.add( MaxPool2D(2,2))
-      model.add( Dropout(0.2))
-    
-      model.add( Conv2D(filters = 64 , kernel_size = (3,3) , padding = 'same' , activation = 'relu' , kernel_initializer = 'he_uniform'))
-      model.add(BatchNormalization())
-      model.add( Conv2D(filters = 64 , kernel_size = (3,3) , padding = 'same' , activation = 'relu' , kernel_initializer = 'he_uniform'))
-      model.add( MaxPool2D(2,2))
-      model.add( Dropout(0.4))
-    
-      model.add( Conv2D(filters = 128 , kernel_size = (3,3) , padding = 'same' , activation = 'relu' , kernel_initializer = 'he_uniform'))
-      model.add(BatchNormalization())
-      model.add( Conv2D(filters = 128 , kernel_size = (3,3) , padding = 'same' , activation = 'relu' , kernel_initializer = 'he_uniform'))
-      model.add( MaxPool2D(2,2))
-      model.add( Dropout(0.5))
-    
-      model.add(Flatten())
-      model.add(Dense(units = 128 , activation = 'relu' , kernel_initializer = 'he_uniform'))
-      model.add(Dense(units = 10 , activation = 'softmax')) 
-      model.compile(optimizer = 'adam' , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
-      History.append(model.fit(x = X[train], y = to_categorical(Y[train]) , epochs=50 , batch_size = 128 , validation_data=(X[test] , to_categorical(Y[test]))))
-      scores = model.evaluate(X[test] , to_categorical(Y[test]))
-      cvscores.append(scores[1] * 100)
-      F1_score.append(f1_score( Y[test] ,  model.predict_classes(X[test]) , average = 'micro') )
-     
-    print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
-    
-    # create data generator
-    datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
-    # prepare iterator
-    it_train = datagen.flow(X_train, y_train, batch_size=64)
-    
-    # fit model
-    steps = int(X_train.shape[0] / 64)
-    history = model.fit_generator(it_train, steps_per_epoch=steps, epochs=100, validation_data=(testX, testY), verbose=0)
-    
-    print("Total Accuracy is %.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
-    print("F1 Score is %.2f%% (+/- %.2f%%)" % (np.mean(F1_score), np.std(F1_score)))
-    
-    #USING MULTI LEVEL PERCEPTRON
-    from keras.optimizers import Adam
-    model = Sequential()
-    model.add(Dense(256, activation='relu', input_dim=(32*32*3)))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(10, activation='softmax'))
-    sgd = Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
-    
-    model.compile(optimizer=sgd,
+
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+cvscores = []
+History = []
+F1_score = []
+for train, test in kfold.split(X, Y):
+  
+  model = Sequential()
+  model.add( Conv2D(input_shape = (32, 32,3) , filters = 32 , kernel_size=(3,3) , padding = 'same' , activation = 'relu' , kernel_initializer= 'he_uniform'))
+  model.add(BatchNormalization())
+  model.add( Conv2D(filters = 32 , kernel_size = (3,3) , padding = 'same' , activation = 'relu' , kernel_initializer = 'he_uniform'))
+  model.add( MaxPool2D(2,2))
+  model.add( Dropout(0.2))
+
+  model.add( Conv2D(filters = 64 , kernel_size = (3,3) , padding = 'same' , activation = 'relu' , kernel_initializer = 'he_uniform'))
+  model.add(BatchNormalization())
+  model.add( Conv2D(filters = 64 , kernel_size = (3,3) , padding = 'same' , activation = 'relu' , kernel_initializer = 'he_uniform'))
+  model.add( MaxPool2D(2,2))
+  model.add( Dropout(0.4))
+
+  model.add( Conv2D(filters = 128 , kernel_size = (3,3) , padding = 'same' , activation = 'relu' , kernel_initializer = 'he_uniform'))
+  model.add(BatchNormalization())
+  model.add( Conv2D(filters = 128 , kernel_size = (3,3) , padding = 'same' , activation = 'relu' , kernel_initializer = 'he_uniform'))
+  model.add( MaxPool2D(2,2))
+  model.add( Dropout(0.5))
+
+  model.add(Flatten())
+  model.add(Dense(units = 128 , activation = 'relu' , kernel_initializer = 'he_uniform'))
+  model.add(Dense(units = 10 , activation = 'softmax')) 
+  model.compile(optimizer = 'adam' , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
+  History.append(model.fit(x = X[train], y = to_categorical(Y[train]) , epochs=50 , batch_size = 128 , validation_data=(X[test] , to_categorical(Y[test]))))
+  scores = model.evaluate(X[test] , to_categorical(Y[test]))
+  cvscores.append(scores[1] * 100)
+  F1_score.append(f1_score( Y[test] ,  model.predict_classes(X[test]) , average = 'micro') )
+ 
+print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+
+# create data generator
+datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+# prepare iterator
+it_train = datagen.flow(X_train, y_train, batch_size=64)
+
+# fit model
+steps = int(X_train.shape[0] / 64)
+history = model.fit_generator(it_train, steps_per_epoch=steps, epochs=100, validation_data=(testX, testY), verbose=0)
+
+print("Total Accuracy is %.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+print("F1 Score is %.2f%% (+/- %.2f%%)" % (np.mean(F1_score), np.std(F1_score)))
+
+#USING MULTI LEVEL PERCEPTRON
+
+model = Sequential()
+model.add(Dense(256, activation='relu', input_dim=(32*32*3)))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+#### Loop over different optimizers
+opttcount = 0
+for algo in optzer:
+    model.compile(optimizer=algo,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
     history = model.fit(X_train.reshape(50000 , 32*32*3),to_categorical(y_train), epochs=15, batch_size=32, verbose=2, validation_split=0.2)
@@ -150,14 +156,14 @@ for nsplt in range(9):
     
     def summarize_diagnostics(history):
     	plt.subplot(211)
-    	plt.title('Cross Entropy Loss')
+    	plt.title('Cross Entropy Loss - Optimizer: ' + optzer_name[opttcount])
     	plt.plot(history.history['loss'], color='blue', label='train')
     	plt.plot(history.history['val_loss'], color='orange', label='test')
     	plt.subplot(212)
-    	plt.title('Classification Accuracy')
+    	plt.title('Classification Accuracy - Optimizer: ' + optzer_name[opttcount])
     	plt.plot(history.history['acc'], color='blue', label='train')
     	plt.plot(history.history['val_acc'], color='orange', label='test')
-    
+    opttcount += 1
     i = 1
     for h in History:
       
